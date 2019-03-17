2ff4afca4800749522edd1d1b61d16b34f63a769
m4mike <mike.hsn@gmail.com>
2019-03-17 02:45:52 +0200

    update: disable lint

--- src/components/Modals/EmailModal.jsx
+++ src/components/Modals/EmailModal.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Modal from "react-animated-modal";
 import "./modal.css"
 import Ratings from 'react-ratings-declarative';
-
+/* eslint-disable */
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
